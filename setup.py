e0279fce9e1d5295e051d30697a3c7367aba8815
dhanushpavann <dhanushb4532@gmail.com>
2026-06-04 20:35:27 +0530

    Project Structure ready,venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Dhanush Pavan",
+    author_email="dhanushb4532@gmail.com",
+    packages=find_packages(),
+)
