d04273142021c12c067c613183f67d11f5e9b25e
dhanushpavann <dhanushb4532@gmail.com>
2026-06-30 19:34:17 +0530

    Feat: AWS SimpleStorageService Configured

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -67,7 +67,7 @@ MODEL_TRAINER_RANDOM_STATE: int = 101
 MODEL Evaluation related constants
 """
 MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
-MODEL_BUCKET_NAME = "my-model-mlopsproj"
+MODEL_BUCKET_NAME = "my-model-mlopsproj-d"
 MODEL_PUSHER_S3_KEY = "model-registry"
 
 
